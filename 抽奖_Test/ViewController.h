3c73b15d4e0e14047f78abfbf6eaa32cdd39c962
dangxy <945979125@qq.com>
2015-11-03 16:10:40 +0800

    抽奖

---
+++ 抽奖_Test/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  抽奖_Test
+//
+//  Created by aaron on 14-7-18.
+//  Copyright (c) 2014年 The Technology Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *label1;
+@property (strong, nonatomic) IBOutlet UITextField *labelTextField;
+@property (strong, nonatomic) IBOutlet UIImageView *plateImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *rotateStaticImageView;
+
+@end
